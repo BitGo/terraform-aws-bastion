b2366dbb012d57a51c0541f6e0d6bd30b85ca968
James Callahan <jamescallahan@bitgo.com>
2022-07-18 16:41:41 +1000

    Remove ignition provider by inlining ignition config

--- ec2.tf
+++ ec2.tf
@@ -16,91 +16,99 @@ data "aws_ami" "flatcar" {
   }
 }
 
-data "ignition_user" "tunnel" {
-  name           = "tunnel"
-  home_dir       = "/dev/shm"
-  no_create_home = true
-  shell          = "/bin/false"
-}
-
-data "ignition_file" "sshd_config" {
-  path       = "/etc/ssh/sshd_config"
-  filesystem = "root"
-  content {
-    content = <<EOF
-AllowUsers ${join(" ", var.allowed_users)}
-AuthenticationMethods publickey
-AuthorizedKeysCommandUser nobody
-AuthorizedKeysCommand /etc/ssh/authorized_keys.sh
-PermitRootLogin no
-PermitTunnel yes
-StreamLocalBindUnlink yes
-EOF
-
-  }
-}
-
-data "ignition_file" "sshd_authorized_keys" {
-  path       = "/etc/ssh/authorized_keys.sh"
-  filesystem = "root"
-  mode       = "493"
-  uid        = "0"
-  gid        = "0"
-  content {
-    content = <<EOF
-#!/bin/bash
-curl -sf "${aws_s3_bucket.ssh_public_keys.website_endpoint}/authorized_keys"
-EOF
-
-  }
-}
-
-data "ignition_systemd_unit" "sshd_port" {
-  name = "sshd.socket"
-  dropin {
-    name    = "10-sshd-port.conf"
-    content = <<EOF
-[Socket]
-ListenStream=
-ListenStream=${var.ssh_port}
-EOF
-
-  }
-}
-
-data "ignition_systemd_unit" "mask_docker" {
-  name = "docker.service"
-  mask = true
-}
-
-data "ignition_systemd_unit" "mask_containerd" {
-  name = "containerd.service"
-  mask = true
-}
-
-data "ignition_config" "bastion" {
-  systemd = [
-    data.ignition_systemd_unit.sshd_port.rendered,
-    data.ignition_systemd_unit.mask_containerd.rendered,
-    data.ignition_systemd_unit.mask_docker.rendered,
-  ]
-  users = [
-    data.ignition_user.tunnel.rendered,
-  ]
-  files = [
-    data.ignition_file.sshd_config.rendered,
-    data.ignition_file.sshd_authorized_keys.rendered,
-  ]
-}
-
 // Just in case something goes wrong, the AMI ID for us-west-2 flatcar is 'ami-0bb54692374ac10a7'
 // Source: https://docs.flatcar-linux.org/os/booting-on-ec2/
 //
 resource "aws_launch_template" "bastion" {
   image_id      = data.aws_ami.flatcar.image_id
   instance_type = var.instance_type
-  user_data     = base64encode(data.ignition_config.bastion.rendered)
-  key_name      = var.key_name
+  user_data = base64encode(jsonencode({
+    ignition = {
+      config   = {}
+      timeouts = {}
+      version  = "2.1.0"
+    },
+    networkd = {}
+    passwd = {
+      users = [
+        {
+          homeDir      = "/dev/shm"
+          name         = "tunnel"
+          noCreateHome = true,
+          shell        = "/bin/false"
+        }
+      ]
+    },
+    storage = {
+      files = [
+        {
+          filesystem = "root"
+          group      = {}
+          path       = "/etc/ssh/sshd_config"
+          user       = {}
+          contents = {
+            source = "data:text/plain;charset=utf-8;base64,${base64encode(
+              <<-EOT
+                AllowUsers ${join(" ", var.allowed_users)}
+                AuthenticationMethods publickey
+                AuthorizedKeysCommandUser nobody
+                AuthorizedKeysCommand /etc/ssh/authorized_keys.sh
+                PermitRootLogin no
+                PermitTunnel yes
+                StreamLocalBindUnlink yes
+              EOT
+            )}"
+            verification = {}
+          }
+        },
+        {
+          filesystem = "root"
+          group      = {}
+          path       = "/etc/ssh/authorized_keys.sh"
+          user       = {}
+          contents = {
+            source = "data:text/plain;charset=utf-8;base64,${base64encode(
+              <<-EOT
+                #!/bin/bash
+                curl -sf "${aws_s3_bucket.ssh_public_keys.website_endpoint}/authorized_keys"
+              EOT
+            )}"
+            verification = {}
+          },
+          mode = 493
+        }
+      ]
+    },
+    systemd = {
+      units = [
+        {
+          dropins = [
+            {
+              contents = <<-EOT
+                [Socket]
+                ListenStream=
+                ListenStream=${var.ssh_port}
+              EOT
+              name     = "10-sshd-port.conf"
+            }
+          ],
+          enabled = true
+          name    = "sshd.socket"
+        },
+        {
+          enabled = true
+          mask    = true
+          name    = "containerd.service"
+        },
+        {
+          enabled = true,
+          mask    = true,
+          name    = "docker.service"
+        }
+      ]
+    }
+  }))
+  key_name = var.key_name
 
   iam_instance_profile {
     name = var.iam_instance_profile
